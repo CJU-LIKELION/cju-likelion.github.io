b2dc92f65f29c0153ed814a782626ed189a8aff5
Eunseong Kim <seolheun36@gmail.com>
2026-02-13 05:00:14 +0900

    style(format): apply prettier to project source

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? ''
+const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
 const base =
-  repositoryName === '' || repositoryName.endsWith('.github.io')
-    ? '/'
-    : `/${repositoryName}/`
+  repositoryName === "" || repositoryName.endsWith(".github.io")
+    ? "/"
+    : `/${repositoryName}/`;
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,7 +15,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
